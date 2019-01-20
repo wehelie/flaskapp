96594944bb1c1f19aa94ad75b868593839a41ad3
Leyth <mawehelie@gmail.com>
2019-01-19 21:39:04 -0800

    first commit

---
+++ data.py
@@ -0,0 +1,26 @@
+def Articles():
+    articles = [
+        {
+            'id':1,
+            'title': 'Article One',
+            'body': 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit, praesentium consectetur ipsam culpa, ipsa sequi mollitia ipsum molestias repudiandae neque animi reiciendis, porro dicta totam! Maxime non eveniet necessitatibus illo.',
+            'create_date': '11-25-2018',
+            'author': 'jo doe'
+        },
+        {
+            'id':2,
+            'title': 'Article Two',
+            'body': 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit, praesentium consectetur ipsam culpa, ipsa sequi mollitia ipsum molestias repudiandae neque animi reiciendis, porro dicta totam! Maxime non eveniet necessitatibus illo.',
+            'create_date': '11-25-2018',
+            'author': 'mahmud adan'
+        },
+        {
+            'id':3,
+            'title': 'Article One',
+            'body': 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit, praesentium consectetur ipsam culpa, ipsa sequi mollitia ipsum molestias repudiandae neque animi reiciendis, porro dicta totam! Maxime non eveniet necessitatibus illo.',
+            'create_date': '12-12-2018',
+            'author': 'leyth adan'
+        }
+    ]
+
+    return articles
